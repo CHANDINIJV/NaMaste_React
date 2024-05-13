1e5d2e30faae7214b03d319a3ac23c9b298c8a39
CHANDINIJV <chandinijv2000@gmail.com>
2024-05-13 14:53:59 +0530

    react-hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,26 @@
 import resList from "../utils/mockData";
 import ResCards from "./Rescard";
+import { useState } from "react";
 
 const Body = () =>{
+
+    const [listOfRes, setListofRes] = useState(resList);
     return (
         <div className="search-container">
             <div className="search">
-                Search
+                <button class="filter-btn"
+                onClick= {() =>{
+                    const filter = listOfRes.filter(
+                        (res) => res.info.avgRating > 4.3 
+                    );
+                    setListofRes(filter);
+                }
+                }
+                >Top Rated Restaurant</button>
             </div>
             <div className="rescard-container">
            {
-            resList.map((restaurant)=>(
+            listOfRes.map((restaurant)=>(
                 <ResCards key={restaurant.info.id} resData={restaurant} />
             ))
             }
